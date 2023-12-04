a8c442119f9298eff2200a55664b2b13d2a04734
FrancoRossiDs <francorosssic@gmail.com>
2023-12-04 15:45:11 -0300

    Subo clase 10

---
+++ 10 - JPA/ProyectoJPA/src/test/java/com/example/PoryectoJPA/PoryectoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PoryectoJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoryectoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
